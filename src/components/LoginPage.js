7cca258119b84f6fdc7ba048e2a231444313d593
P.Sujith <sujithpappala777@gmail.com>
2025-03-25 12:08:35 +0530

    done with how toimplement and i need to make more whiich are page and custom button

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,13 +1,23 @@
 import React from "react";
 import "./LoginPage.css"
+import { GoogleLogin } from '@react-oauth/google';
+import { jwtDecode } from "jwt-decode";
 
 const Login = () => {
-    return(
+    return (
         <div className="logintext">
             this is login  brooo
             <button className="logindabba">
+            <GoogleLogin
+                onSuccess={credentialResponse => {
 
-                google
+                    const jetdecoderesponseafterdecode = jwtDecode(credentialResponse.credential)
+                    console.log(jetdecoderesponseafterdecode);
+                }}
+                onError={() => {
+                    console.log('Login Failed');
+                }}
+            />
 
             </button>
         </div>
--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import jwt_decode from 'jwt-decode';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+
+<GoogleOAuthProvider clientId="600709826330-nnjdnfhobhh2nmi7m1hn2h8lpplto8q5.apps.googleusercontent.com">
+  
     <App />
+  </GoogleOAuthProvider>;
   </React.StrictMode>
 );
 
