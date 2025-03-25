e1ab4a7959499dc87c3db6377776463a3786f0c5
P.Sujith <sujithpappala777@gmail.com>
2025-03-25 11:03:37 +0530

    done setup to start login with oauth2

---
+++ src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./HomePage.css"
+
+const Home = () => {
+    return(
+        <div>
+            this is home.js  broooo
+        </div>
+
+
+    );
+}
+
+export default Home;
